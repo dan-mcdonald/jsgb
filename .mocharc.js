0cad7f668daec289d0e7085539e925f9cf8a2fe0
Daniel McDonald <daniel@mcd.onald.net>
2023-09-06 18:48:58 +0300

    test: max timeout 30s, parallel execution

--- .mocharc.js
+++ .mocharc.js
@@ -4,5 +4,6 @@ module.exports = {
   require: ['ts-node/register'],
   spec: ['test/**/*.spec.ts'],
   'watch-files': ['src/**/*.ts', 'test/**/*.ts'],
-  timeout: 10000,
+  timeout: '30000',
+  parallel: true,
 };
