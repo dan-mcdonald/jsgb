9547e22423fcfa468b3f2adf212019ba60a8faae
Daniel McDonald <daniel@mcd.onald.net>
2023-09-29 19:53:13 +0300

    test: watch test fixtures

--- .mocharc.js
+++ .mocharc.js
@@ -3,7 +3,7 @@
 module.exports = {
   require: ['ts-node/register'],
   spec: ['test/**/*.spec.ts'],
-  'watch-files': ['src/**/*.ts', 'test/**/*.ts'],
+  'watch-files': ['src/**/*.ts', 'test/**/*.ts', 'test/fixtures/**/*'],
   timeout: '30000',
   parallel: true,
 };
